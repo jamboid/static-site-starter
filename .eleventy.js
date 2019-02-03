34aee8928d9fa45b09b7848cc9c9d9b960ee69f8
Jamie Boyd <enkizero@gmail.com>
2019-02-03 08:37:50 +0000

    Added some global data variables

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,8 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "./source", // Equivalent to Jekyll's source property
-      output: "./_site" // Equivalent to Jekyll's destination property
+      output: "./_site", // Equivalent to Jekyll's destination property
+      data: "_data"
     }
   };
 };
--- source/index.md
+++ source/index.md
@@ -1,6 +1,6 @@
 ---
 layout: home
-title: Chris Boyd Design
+title: Home
 --- 
 
 # Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ea culpa laudantium. Ut in ducimus dolore. Dicta nihil nisi libero aperiam, non, culpa nobis officia aut distinctio odit neque quis.
