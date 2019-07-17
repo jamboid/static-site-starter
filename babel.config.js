26191420cdfa42902917d48f3da362f8d06da7a5
Jamie Boyd <enkizero@gmail.com>
2019-07-17 23:03:13 +0100

    Updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [];
+  const presets = [
+    ['@babel/preset-env', {
+      targets: {
+        ie: 11,
+        browsers: 'last 2 versions'
+      },
+      useBuiltIns: 'usage',
+      debug: true
+    }]
+  ];
   const plugins = [];
 
   return {
